a9f24247e7e607ef46ada194f88f00055bab191a
lMuffinn <mattheweagleman@gmail.com>
2024-10-09 14:56:44 -0500

    Started lab

---
+++ problem1.c
@@ -0,0 +1,15 @@
+/*
+Sales Report
+Date Created: 10-9-2024
+Author: Matthew Eagleman
+Description: A program that gives you info about sales figures
+Input: Twelve monthly sales figures
+Output: Various facts about said figures
+ */
+
+int main(){
+    int info[12][2];
+    for (int i = 0;i<12;i++){
+        info[i][0] = i+1;
+    }
+}
